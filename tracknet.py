669ab0af9a30b33879219eaea796588fbd9ce39a
Bartek <pony830906@gmail.com>
2024-09-08 21:21:59 +0800

    test

--- tracknet.py
+++ tracknet.py
@@ -81,6 +81,7 @@ def main(arg):
             model.cuda()
             worker = 1
         dataset = TrackNetTestDataset(root_dir=arg.source)
+        print(arg.source)
         print((arg.batch, len(dataset)))
         dataloader = build_dataloader(dataset, arg.batch, worker, shuffle=False, rank=-1)
         overrides = overrides.copy()
