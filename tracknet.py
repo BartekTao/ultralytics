f1aa0ac223bf65f43739bd888eaaa2c16ba733dd
Bartek <pony830906@gmail.com>
2024-09-22 19:02:13 +0800

    test

--- tracknet.py
+++ tracknet.py
@@ -306,7 +306,7 @@ def main(arg):
                     conf_FN += 0  # No false negatives
             else:
                 # Compute confusion matrix normally
-                conf_matrix = confusion_matrix(cls_targets.bool().cpu().numpy(), pred_binary.cpu().numpy())
+                conf_matrix = confusion_matrix_gpu(cls_targets.bool().cpu().numpy(), pred_binary.cpu().numpy())
                 conf_TN += conf_matrix[0][0]
                 conf_FP += conf_matrix[0][1]
                 conf_FN += conf_matrix[1][0]
@@ -345,7 +345,7 @@ def main(arg):
                     pos_FN += 0  # No false negatives
             else:
                 # Compute confusion matrix normally
-                pos_matrix = confusion_matrix(ground_truth_binary_tensor.cpu().numpy(), tensor_combined_correct.cpu().numpy())
+                pos_matrix = confusion_matrix_gpu(ground_truth_binary_tensor.cpu().numpy(), tensor_combined_correct.cpu().numpy())
                 pos_TN += pos_matrix[0][0]
                 pos_FP += pos_matrix[0][1]
                 pos_FN += pos_matrix[1][0]
@@ -521,6 +521,14 @@ def main(arg):
                     )
 
         print(f"avg predict time: { elapsed_times / len(dataloader):.2f} 毫秒")    
+
+def confusion_matrix_gpu(y_true, y_pred, num_classes):
+    conf_matrix = torch.zeros(num_classes, num_classes, dtype=torch.int64, device=y_true.device)
+    
+    for t, p in zip(y_true.view(-1), y_pred.view(-1)):
+        conf_matrix[t.long(), p.long()] += 1
+    
+    return conf_matrix
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Train a custom model with overrides.')
