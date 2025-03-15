e5a9249894d77dd1eef57db7f6630965dba3fb8b
BartekTao <bartek_tao@cmoney.com.tw>
2025-03-15 09:02:07 +0800

    fix: next ball transform

--- ultralytics/tracknet/val.py
+++ ultralytics/tracknet/val.py
@@ -1923,8 +1923,8 @@ class TrackNetValidator(BaseValidator):
             p_cell_x = each_pos_x[frame_idx]
             p_cell_y = each_pos_y[frame_idx]
 
-            p_cell_nx = each_pos_x[frame_idx]
-            p_cell_ny = each_pos_y[frame_idx]
+            p_cell_nx = each_pos_nx[frame_idx]
+            p_cell_ny = each_pos_ny[frame_idx]
             metrics = []
             # 獲取當前圖片的 conf
             p_conf = each_probs[frame_idx]
