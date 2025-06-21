03546ebc8d27c208d3dd1b2c32af881887d34a45
BartekTao <bartek_tao@cmoney.com.tw>
2025-06-21 11:28:16 +0800

    close top k loss

--- ultralytics/tracknet/utils/loss.py
+++ ultralytics/tracknet/utils/loss.py
@@ -898,13 +898,13 @@ class FocalLossWithMask(nn.Module):
         FP_mask = (pred_prob >= 0.5) & (label == 0)  # False Positive
 
         # Combine the masks (we only care about TP, FN, FP)
-        relevant_mask = self.online_hard_example_mining(loss, label, negative_ratio)
+        # relevant_mask = self.online_hard_example_mining(loss, label, negative_ratio)
 
         pos_no = label.sum() if label.sum() != 0 else 1
 
         w = (alpha/(1-alpha))
 
-        loss = loss * relevant_mask.float()
+        # loss = loss * relevant_mask.float()
 
         # loss[FN_mask] *= negative_ratio*20*w
         # loss[FP_mask & ~may_has_ball] *= negative_ratio*15*w
