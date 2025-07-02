3f9960fab7186f69f6c0ad6c6179960c19b87ec4
BartekTao <bartek_tao@cmoney.com.tw>
2025-07-02 22:13:07 +0800

    feat: adjust inference preprocess - remove .clamp_(0, 255)

--- ultralytics/tracknet/predict.py
+++ ultralytics/tracknet/predict.py
@@ -102,7 +102,7 @@ class TrackNetPredictor(BasePredictor):
 
         im = im.to(self.device, dtype=torch.float32, non_blocking=True)
         median = im.median(dim=1).values  # shape: (H, W)
-        im.sub_(median).clamp_(0, 255).div_(255.0)
+        im.sub_(median).div_(255.0)
 
         # im = im.unsqueeze(0)
 
