6a2b33e3e0303f0656e0755833d9946f8580dd3b
BartekTao <bartek_tao@cmoney.com.tw>
2025-07-13 18:02:56 +0800

    update CSV output to append data and use a fixed filename

--- ultralytics/tracknet/predict.py
+++ ultralytics/tracknet/predict.py
@@ -362,10 +362,11 @@ class TrackNetPredictor(BasePredictor):
                     })
 
         # 儲存 csv
-        idx_csv = f'{int(p.stem) + frame_idx}.csv'
+        idx_csv = f'all.csv'
         save_csv_path = f"{csv_save_path}/{idx_csv}"
         df = pd.DataFrame(csv_rows)
-        df.to_csv(save_csv_path, index=False, encoding="utf-8")
+        write_header = not os.path.exists(save_csv_path)
+        df.to_csv(save_csv_path, mode='a', header=write_header, index=False, encoding="utf-8")
                 
         if self.mqttc is not None and self.output_topic is not None:
             # Publish the results to MQTT
